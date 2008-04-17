8ce948cc57829ae8964358b1c5b8d9921e2c5ae0
liu21st@gmail.com <liu21st@gmail.com@539fd3ec-2725-0410-b7ab-251e373a8e33>
2008-04-17 03:08:03 +0000

    修改命令行自动创建工具build

--- trunk/ThinkPHP/Tools/config.php
+++ trunk/ThinkPHP/Tools/config.php
@@ -2,12 +2,15 @@
 //ThinkPHP系统目录定义
 define('THINK_PATH', '../../ThinkPHP');
 define('APP_PATH','../');
-define('DB_TYPE','mysql'); 
-define('DB_HOST','localhost'); 
-define('DB_NAME','thinkcms'); 
-define('DB_USER','root'); 
-define('DB_PWD',''); 
-define('DB_PORT','3306'); 
-define('DB_PREFIX','think_'); 
+// 数据库连接信息
+$config	=	array(
+	'DB_TYPE'=>'mysql', 
+	'DB_HOST'=>'localhost', 
+	'DB_NAME'=>'test',
+	'DB_USER'=>'root', 
+	'DB_PWD'=>'',
+	'DB_PORT'=>'3306',
+	'DB_PREFIX'=>'think_',
+)
 
 ?>
