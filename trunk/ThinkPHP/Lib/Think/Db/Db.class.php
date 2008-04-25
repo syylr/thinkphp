ec633f3edabc0db0708f841c4c8d48611075eba0
liu21st@gmail.com <liu21st@gmail.com@539fd3ec-2725-0410-b7ab-251e373a8e33>
2008-04-25 08:53:07 +0000

    增加数据查询的IS NULL 和 IS NOT NULL支持

--- trunk/ThinkPHP/Lib/Think/Db/Db.class.php
+++ trunk/ThinkPHP/Lib/Think/Db/Db.class.php
@@ -506,7 +506,7 @@ class Db extends Base
 									}elseif(is_string($val[$i][0]) && preg_match('/IN/i',$val[$i][0])){
 										$zone	=	is_array($val[$i][1])? implode(',',$val[$i][1]):$val[$i][1];
 										$str[] =  ' ('.$key[$i].' '.strtoupper($val[$i][0]).' ('.$zone.') )';
-									}elseif(is_string($val[$i][0]) && preg_match('/^(EQ|NEQ|GT|EGT|LT|ELT|LIKE)$/i',$val[$i][0])){
+									}elseif(is_string($val[$i][0]) && preg_match('/^(EQ|NEQ|GT|EGT|LT|ELT|LIKE|NULL|NOTNULL|BETWEEN)$/i',$val[$i][0])){
 										$op	=	$this->comparison[strtolower($val[$i][0])];
 										$str[] = ' ('.$key[$i].' '.$op.' '.$this->fieldFormat($val[$i][1]).') ';
 									}else{
@@ -519,7 +519,7 @@ class Db extends Base
 							}
 							$whereStr .= implode(strtoupper($val[$num]),$str);
 						}else{
-							if(is_string($val[0]) && preg_match('/^(EQ|NEQ|GT|EGT|LT|ELT|LIKE|BETWEEN)$/i',$val[0])) {
+							if(is_string($val[0]) && preg_match('/^(EQ|NEQ|GT|EGT|LT|ELT|LIKE|NULL|NOTNULL|BETWEEN)$/i',$val[0])) {
 								// 是否存在比较运算
 								$whereStr .= $key.' '.$this->comparison[strtolower($val[0])].' '.$this->fieldFormat($val[1]);
 							}elseif(is_string($val[0]) && preg_match('/IN/i',$val[0])){
