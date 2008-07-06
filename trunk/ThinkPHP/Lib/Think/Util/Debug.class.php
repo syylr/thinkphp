edb2acae4f3266b750f623a6b6a3a2d1c13917ad
liu21st@gmail.com <liu21st@gmail.com@539fd3ec-2725-0410-b7ab-251e373a8e33>
2008-07-06 01:33:52 +0000

    Debug类支持内存占用峰值查看

--- trunk/ThinkPHP/Lib/Think/Util/Debug.class.php
+++ trunk/ThinkPHP/Lib/Think/Util/Debug.class.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 // +----------------------------------------------------------------------
-// | ThinkPHP                                                             
+// | ThinkPHP
 // +----------------------------------------------------------------------
-// | Copyright (c) 2008 http://thinkphp.cn All rights reserved.      
+// | Copyright (c) 2008 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>                                  
+// | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 // $Id$
 
@@ -29,18 +29,19 @@ class Debug extends Base
      +----------------------------------------------------------
      * 标记调试位
      +----------------------------------------------------------
-     * @access public 
+     * @access public
      +----------------------------------------------------------
      * @param string $name  要标记的位置名称
      +----------------------------------------------------------
      * @return void
      +----------------------------------------------------------
      */
-    static public function mark($name) 
+    static public function mark($name)
     {
     	self::$marker['time'][$name]  =  microtime();
         if(MEMORY_LIMIT_ON) {
-        	 self::$marker['mem'][$name] = memory_get_usage();
+        	self::$marker['mem'][$name] = memory_get_usage();
+            self::$marker['peak'][$name] = function_exists('memory_get_peak_usage')?memory_get_peak_usage(): self::$marker['mem'][$name];
         }
     }
 
@@ -48,26 +49,26 @@ class Debug extends Base
      +----------------------------------------------------------
      * 区间使用时间查看
      +----------------------------------------------------------
-     * @access public 
+     * @access public
      +----------------------------------------------------------
      * @param string $start  开始标记的名称
      * @param string $end  结束标记的名称
      * @param integer $decimals  时间的小数位
      +----------------------------------------------------------
-     * @return void
+     * @return integer
      +----------------------------------------------------------
      */
-    static public function useTime($start,$end,$decimals = 6) 
+    static public function useTime($start,$end,$decimals = 6)
     {
 		if ( ! isset(self::$marker['time'][$start]))
 		{
 			return '';
 		}
-		
+
 		if ( ! isset(self::$marker['time'][$end]))
 		{
 			self::$marker['time'][$end] = microtime();
-		}    	
+		}
         $startTime    =  array_sum(explode(' ', self::$marker['time'][$start]));
         $endTime     =  array_sum(explode(' ', self::$marker['time'][$end]));
         return number_format($endTime - $startTime, $decimals);
@@ -77,15 +78,15 @@ class Debug extends Base
      +----------------------------------------------------------
      * 区间使用内存查看
      +----------------------------------------------------------
-     * @access public 
+     * @access public
      +----------------------------------------------------------
      * @param string $start  开始标记的名称
      * @param string $end  结束标记的名称
      +----------------------------------------------------------
-     * @return void
+     * @return integer
      +----------------------------------------------------------
      */
-    static public function useMemory($start,$end) 
+    static public function useMemory($start,$end)
     {
         if(!MEMORY_LIMIT_ON) {
         	return '';
@@ -94,15 +95,41 @@ class Debug extends Base
 		{
 			return '';
 		}
-		
+
 		if ( ! isset(self::$marker['mem'][$end]))
 		{
 			self::$marker['mem'][$end] = memory_get_usage();
-		}    	
+		}
         $startMem    =  array_sum(explode(' ', self::$marker['mem'][$start]));
         $endMem     =  array_sum(explode(' ', self::$marker['mem'][$end]));
         return number_format(($endMem - $startMem)/1024);
     }
 
+    /**
+     +----------------------------------------------------------
+     * 区间使用内存峰值查看
+     +----------------------------------------------------------
+     * @access public
+     +----------------------------------------------------------
+     * @param string $start  开始标记的名称
+     * @param string $end  结束标记的名称
+     +----------------------------------------------------------
+     * @return integer
+     +----------------------------------------------------------
+     */
+    static function getMemPeak($start,$end) {
+        if(!MEMORY_LIMIT_ON) {
+        	return '';
+        }
+		if ( ! isset(self::$marker['peak'][$start]))
+		{
+			return '';
+		}
+		if ( ! isset(self::$marker['peak'][$end]))
+		{
+			self::$marker['peak'][$end] = function_exists('memory_get_peak_usage')?memory_get_peak_usage(): memory_get_usage();
+		}
+        return number_format(max(self::$marker['peak'][$start],self::$marker['peak'][$end])/1024);
+    }
 }//类定义结束
 ?>
