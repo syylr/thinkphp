ec0b734b2e3d8078cace21b679ee14d2d42e9227
liu21st <liu21st@gmail.com>
2008-07-30 09:41:22 +0000

    修正在php6下面的get_magic_quotes_gpc错误

--- ThinkPHP/Common/defines.php
+++ ThinkPHP/Common/defines.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 // +----------------------------------------------------------------------
-// | ThinkPHP                                                             
+// | ThinkPHP
 // +----------------------------------------------------------------------
-// | Copyright (c) 2008 http://thinkphp.cn All rights reserved.      
+// | Copyright (c) 2008 http://thinkphp.cn All rights reserved.
 // +----------------------------------------------------------------------
 // | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
 // +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>                                  
+// | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 // $Id$
 
@@ -22,8 +22,10 @@
  */
 if (!defined('THINK_PATH')) exit();
 //	 系统信息
-@set_magic_quotes_runtime (0);
-define('MAGIC_QUOTES_GPC',get_magic_quotes_gpc()?True:False);
+if(version_compare(PHP_VERSION,'6.0.0','<') ) {
+	@set_magic_quotes_runtime (0);
+	define('MAGIC_QUOTES_GPC',get_magic_quotes_gpc()?True:False);
+}
 define('OUTPUT_GZIP_ON',ini_get('output_handler') || ini_get('zlib.output_compression') );
 define('MEMORY_LIMIT_ON',function_exists('memory_get_usage')?true:false);
 // 记录内存初始使用
@@ -64,17 +66,17 @@ define('VENDOR_PATH',THINK_PATH.'/Vendor/');
 set_include_path(get_include_path() . PATH_SEPARATOR . VENDOR_PATH);
 
 // 目录设置
-define('CACHE_DIR',  'Cache'); 
-define('HTML_DIR',    'Html'); 
+define('CACHE_DIR',  'Cache');
+define('HTML_DIR',    'Html');
 define('CONF_DIR',    'Conf');
 define('LIB_DIR',        'Lib');
 define('LOG_DIR',      'Logs');
 define('LANG_DIR',    'Lang');
 define('TEMP_DIR',    'Temp');
-define('TMPL_DIR',     'Tpl'); 
+define('TMPL_DIR',     'Tpl');
 // 路径设置
 if (!defined('ADMIN_PATH')) define('ADMIN_PATH', APP_PATH.'/../Admin/');
-define('TMPL_PATH',APP_PATH.'/'.TMPL_DIR.'/'); 
+define('TMPL_PATH',APP_PATH.'/'.TMPL_DIR.'/');
 define('HTML_PATH',APP_PATH.'/'.HTML_DIR.'/'); //
 define('COMMON_PATH',	APP_PATH.'/Common/'); // 项目公共目录
 define('LIB_PATH',         APP_PATH.'/'.LIB_DIR.'/'); //
