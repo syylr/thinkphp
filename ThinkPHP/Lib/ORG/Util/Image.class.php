7d57d84d67acf32465b635a03aa2f3f95f689788
liu21st <liu21st@gmail.com>
2008-08-31 08:28:54 +0000

    Image类库添加水印支持

--- ThinkPHP/Lib/ORG/Util/Image.class.php
+++ ThinkPHP/Lib/ORG/Util/Image.class.php
@@ -431,6 +431,66 @@ class Image extends Base
         Image::output($im,$type);
     }
 
+    /**
+     +----------------------------------------------------------
+     * 为图片添加水印
+     +----------------------------------------------------------
+     * @static public
+     +----------------------------------------------------------
+     * @param string $source 原文件名
+     * @param string $water  水印图片
+     * @param string $$savename  添加水印后的图片名
+     * @param string $alpha  水印的透明度
+     +----------------------------------------------------------
+     * @return string
+     +----------------------------------------------------------
+     * @throws ThinkExecption
+     +----------------------------------------------------------
+     */
+
+    static public function water($source,$water,$savename=null,$alpha=80)
+    {
+        //检查文件是否存在
+        if(!file_exists($source)||!file_exists($water))
+            return false;
+
+        //图片信息
+        $sInfo=self::getImageInfo($source);
+        $wInfo=self::getImageInfo($water);
+
+        //如果图片小于水印图片，不生成图片
+        if($sInfo["width"]<$wInfo["width"] || $sInfo['height']<$wInfo['height'])
+            return false;
+
+        //建立图像
+        $sCreateFun="imagecreatefrom".$sInfo['type'];
+        $sImage=$sCreateFun($source);
+        $wCreateFun="imagecreatefrom".$wInfo['type'];
+        $wImage=$wCreateFun($water);
+
+        //设定图像的混色模式
+        imagealphablending($wImage, true);
+
+        //图像位置,默认为右下角右对齐
+        $posY=$sInfo["height"]-$wInfo["height"];
+        $posX=$sInfo["width"]-$wInfo["width"];
+
+        //生成混合图像
+         imagecopymerge($sImage, $wImage, $posX, $posY, 0, 0, $wInfo['width'],$wInfo['height'],$alpha);
+
+        //输出图像
+        $ImageFun='Image'.$sInfo['type'];
+        //如果没有给出保存文件名，默认为原图像名
+        if(!$savename){
+            $savename=$source;
+            @unlink($source);
+        }
+        //保存图像
+        $ImageFun($sImage,$savename);
+        imagedestroy($sImage);
+
+    }
+
     static function output($im,$type='png')
     {
         header("Content-type: image/".$type);
