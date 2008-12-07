48c6dff9e9250b96ee902cc3955388f080866b56
liu21st <liu21st@gmail.com>
2008-12-07 10:24:37 +0000

    mk_dir函数从runtime.php移动到functions.php

--- ThinkPHP/Common/runtime.php
+++ ThinkPHP/Common/runtime.php
@@ -10,13 +10,6 @@
 // +----------------------------------------------------------------------
 // $Id$
 
-function mk_dir($dir, $mode = 0755)
-{
-  if (is_dir($dir) || @mkdir($dir,$mode)) return true;
-  if (!mk_dir(dirname($dir),$mode)) return false;
-  return @mkdir($dir,$mode);
-}
-
 function mkdirs($dirs,$mode=0777) {
     if(is_string($dirs)) {
         $dirs  = explode(',',$dirs);
