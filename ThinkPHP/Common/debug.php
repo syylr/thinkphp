f210052cb1b74410800ceabfa5e8c90d11841aa7
liu21st <liu21st@gmail.com>
2008-12-24 05:41:22 +0000

    修改默认的调试配置文件，在调试模式下面不再生成数据表字段缓存。

--- ThinkPHP/Common/debug.php
+++ ThinkPHP/Common/debug.php
@@ -39,6 +39,7 @@ return  array(
     /* 数据库设置 */
     'DB_DEPLOY_TYPE'=>0,            // 数据库部署方式 0 集中式（单一服务器） 1 分布式（主从服务器）
     'SQL_DEBUG_LOG'=>true,          // 记录SQL语句到日志文件
+    'DB_FIELDS_CACHE'=>false,      // 不缓存数据表的字段信息
 
     /* 数据缓存设置 */
     'DATA_CACHE_TIME'=>-1,          // 数据缓存有效期
