b6f5a20de14130544a8d2156c39c1c1f28ea5ee6
tdweb2u <tdweb2u@539fd3ec-2725-0410-b7ab-251e373a8e33>
2009-02-01 02:37:30 +0000

    日志级别加入中文注释

--- branches/Lite/1.5/Lib/Think/Core/Log.class.php
+++ branches/Lite/1.5/Lib/Think/Core/Log.class.php
@@ -24,16 +24,16 @@
 class Log extends Base
 {//类定义开始
 
-    // 日志级别
-    const EMERG   = 'EMERG';  // Emergency: system is unusable
-    const ALERT    = 'ALERT';  // Alert: action must be taken immediately
-    const CRIT      = 'CRIT';  // Critical: critical conditions
-    const ERR       = 'ERR';  // Error: error conditions
-    const WARN    = 'WARN';  // Warning: warning conditions
-    const NOTICE  = 'NOTIC';  // Notice: normal but significant condition
-    const INFO     = 'INFO';  // Informational: informational messages
-    const DEBUG   = 'DEBUG';  // Debug: debug messages
-    const SQL       = 'SQL';  // SQL：sql messages
+    // 日志级别 从上到下，由低到高
+    const EMERG   = 'EMERG';  // 严重错误: 导致系统崩溃无法使用
+    const ALERT    = 'ALERT';  // 警戒性错误: 必须被立即修改的错误
+    const CRIT      = 'CRIT';  // 临界值错误: 超过临界值的错误，例如一天24小时，而输入的是25小时这样
+    const ERR       = 'ERR';  // 一般错误: 一般性错误
+    const WARN    = 'WARN';  // 警告性错误: 需要发出警告的错误
+    const NOTICE  = 'NOTIC';  // 通知: 程序可以运行但是还不够完美的错误
+    const INFO     = 'INFO';  // 信息: 程序输出信息
+    const DEBUG   = 'DEBUG';  // 调试: 调试信息
+    const SQL       = 'SQL';  // SQL：SQL语句
 
     // 日志记录方式
     const SYSTEM = 0;
