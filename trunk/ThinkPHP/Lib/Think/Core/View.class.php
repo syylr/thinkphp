903790994ccb4b0f9c977e10354438b857b3dbd3
liu21st@gmail.com <liu21st@gmail.com@539fd3ec-2725-0410-b7ab-251e373a8e33>
2009-02-13 01:41:39 +0000

    优化View类的模板变量替换方法

--- trunk/ThinkPHP/Lib/Think/Core/View.class.php
+++ trunk/ThinkPHP/Lib/Think/Core/View.class.php
@@ -337,8 +337,8 @@ class View extends Base
     private function parseTemplatePath($content) {
         // 特殊变量替换
         //项目公共目录
-        $content = str_ireplace(
-            array('../public',   '__PUBLIC__',  '__TMPL__', '__ROOT__',  '__APP__',  '__URL__',   '__ACTION__', '__SELF__'),
+        $content = str_replace(
+            array('../Public',   '__PUBLIC__',  '__TMPL__', '__ROOT__',  '__APP__',  '__URL__',   '__ACTION__', '__SELF__'),
             array(APP_PUBLIC_URL,WEB_PUBLIC_URL,APP_TMPL_URL,__ROOT__,__APP__,__URL__,__ACTION__,__SELF__),
             $content);
         if(C('THINK_PLUGIN_ON')) {
