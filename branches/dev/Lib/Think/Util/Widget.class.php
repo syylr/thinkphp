fbff0ce22a10e815b54e59cd6a55aaea76ae9757
yhustc <yhustc@539fd3ec-2725-0410-b7ab-251e373a8e33>
2009-03-15 15:58:13 +0000

    Widget.class.php中的__CLASS__改为get_class($this),测试发现__CLASS__返回的是基类命名，无法正确的组装模板名称

--- branches/dev/Lib/Think/Util/Widget.class.php
+++ branches/dev/Lib/Think/Util/Widget.class.php
@@ -21,7 +21,7 @@ abstract class Widget extends Base {
         ob_implicit_flush(0);
         if(!file_exists_case($templateFile)){
             // 自动定位模板文件
-            $filename   =  empty($templateFile)?substr(__CLASS__,0,-6):$templateFile;
+            $filename   =  empty($templateFile)?substr(get_class($this),0,-6):$templateFile;
             $templateFile = LIB_PATH.'Widget/'.$filename.C('TEMPLATE_SUFFIX');
             if(!file_exists_case($templateFile)){
                 throw_exception(L('_TEMPLATE_NOT_EXIST_').'['.$templateFile.']');
