9e073a443fb57a572fe57a15d72464ddac84c312
liu21st@gmail.com <liu21st@gmail.com@539fd3ec-2725-0410-b7ab-251e373a8e33>
2009-05-02 04:04:08 +0000

    增加简洁模式扩展

---
+++ trunk/AddOns/Modes/thin.php
@@ -0,0 +1,22 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP
+// +----------------------------------------------------------------------
+// | Copyright (c) 2009 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+// $Id$
+
+// 简洁模式核心定义文件列表
+return array(
+    THINK_PATH.'/Lib/Think/Exception/ThinkException.class.php',// 异常处理
+    THINK_PATH.'/Lib/Think/Core/Log.class.php',// 日志处理
+    THINK_PATH.'/Mode/Thin/App.class.php', // 应用程序类
+    THINK_PATH.'/Mode/Thin/Action.class.php',// 控制器类
+    THINK_PATH.'/Mode/Thin/Model.class.php',// 模型类
+    THINK_PATH.'/Common/alias.php',  // 加载别名
+);
+?>
