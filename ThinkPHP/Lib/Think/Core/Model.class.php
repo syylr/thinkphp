5995b833408aa7ea7f23ca30ee029a49a8eaacf8
liu21st <liu21st@gmail.com>
2009-07-31 10:13:48 +0000

    增加simple_file_read simple_file_save函数 并作为Model类内置使用的字段缓存存取方式

--- ThinkPHP/Lib/Think/Core/Model.class.php
+++ ThinkPHP/Lib/Think/Core/Model.class.php
@@ -110,7 +110,7 @@ class Model extends Think implements IteratorAggregate
         if(empty($this->fields)) {
             // 如果数据表字段没有定义则自动获取
             if(C('DB_FIELDS_CACHE')) {
-                $this->fields = F('_fields/'.$this->name);
+                $this->fields = simple_file_read('_fields/'.$this->name);
                 if(!$this->fields)   $this->flush();
             }else{
                 // 每次都会读取数据表信息
@@ -144,7 +144,7 @@ class Model extends Think implements IteratorAggregate
         // 2008-3-7 增加缓存开关控制
         if(C('DB_FIELDS_CACHE'))
             // 永久缓存数据表信息
-            F('_fields/'.$this->name,$this->fields);
+            simple_file_save('_fields/'.$this->name,$this->fields);
     }
 
     /**
