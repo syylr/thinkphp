4d2dc217473f90fdba63d5ad7ae5184e23c5fdae
liu21st@gmail.com <liu21st@gmail.com@539fd3ec-2725-0410-b7ab-251e373a8e33>
2009-07-31 10:52:30 +0000

    ThinkPHP.php 去掉App::run() 便于一些应用可以单独执行App::init()和进行一些其他的流程控制

--- trunk/ThinkPHP/ThinkPHP.php
+++ trunk/ThinkPHP/ThinkPHP.php
@@ -26,9 +26,7 @@ if(defined('RUNTIME_ALLINONE') && is_file(RUNTIME_PATH.'~allinone.php')) {
     // 自动设置为运行模式
     define('RUNTIME_MODEL',true);
 }else{
-    if(version_compare(PHP_VERSION,'5.0.0','<') ) {
-        die('require PHP > 5.0 !');
-    }
+    if(version_compare(PHP_VERSION,'5.0.0','<'))  die('require PHP > 5.0 !');
     // ThinkPHP系统目录定义
     if(!defined('THINK_PATH')) define('THINK_PATH', dirname(__FILE__));
     if(!defined('APP_NAME')) define('APP_NAME', basename(dirname($_SERVER['SCRIPT_FILENAME'])));
@@ -44,6 +42,4 @@ if(defined('RUNTIME_ALLINONE') && is_file(RUNTIME_PATH.'~allinone.php')) {
 }
 // 记录加载文件时间
 $GLOBALS['_loadTime'] = microtime(TRUE);
-// 执行应用
-App::run();
 ?>
