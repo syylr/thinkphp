275a3416be3fba50fc12aa514f7287494122acf2
liu21st <liu21st@gmail.com>
2009-08-13 08:48:13 +0000

    内置模式扩展完善 Cli:命令模式 支持原生SQL操作 Lite:精简模式 可以支持CURD和连贯操作以及常用查询 模板支持php模板 Thin:简洁模式 只是支持原生SQL操作 不带视图支持（ 自己完成）

---
+++ ThinkPHP/Mode/Lite/alias.php
@@ -0,0 +1,21 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP
+// +----------------------------------------------------------------------
+// | Copyright (c) 2008 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+// $Id$
+
+// 导入别名定义
+alias_import(array(
+    'Model'         =>   THINK_PATH.'/Mode/Lite/Model.class.php',
+    'Db'                  =>    THINK_PATH.'/Mode/Lite/Db.class.php',
+    'Debug'              =>    THINK_PATH.'/Lib/Think/Util/Debug.class.php',
+    'Session'             =>   THINK_PATH.'/Lib/Think/Util/Session.class.php',
+    )
+);
+?>
