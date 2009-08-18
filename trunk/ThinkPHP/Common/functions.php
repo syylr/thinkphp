f19f3574adee31a904251334db886c07c523ed28
liu21st@gmail.com <liu21st@gmail.com@539fd3ec-2725-0410-b7ab-251e373a8e33>
2009-08-18 03:41:42 +0000

    dump改回对xdebug的判断支持 完善Model类的select、delete对于主键的操作

--- trunk/ThinkPHP/Common/functions.php
+++ trunk/ThinkPHP/Common/functions.php
@@ -225,8 +225,10 @@ function dump($var, $echo=true,$label=null, $strict=true)
         ob_start();
         var_dump($var);
         $output = ob_get_clean();
-        $output = preg_replace("/\]\=\>\n(\s+)/m", "] => ", $output);
-        $output = '<pre>'. $label. htmlspecialchars($output, ENT_QUOTES). '</pre>';
+        if(!extension_loaded('xdebug')) {
+            $output = preg_replace("/\]\=\>\n(\s+)/m", "] => ", $output);
+            $output = '<pre>'. $label. htmlspecialchars($output, ENT_QUOTES). '</pre>';
+        }
     }
     if ($echo) {
         echo($output);
