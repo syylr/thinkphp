3c0e57113d60da89f3132a3de97ebd99ecb52193
liu21st@gmail.com <liu21st@gmail.com@539fd3ec-2725-0410-b7ab-251e373a8e33>
2009-08-19 03:49:58 +0000

    原AdvModel类的search toTree和sortBy方法 移入扩展函数库 供项目调用 改名为search_list list_to_tree 和list_sort_by

--- trunk/ThinkPHP/Common/extend.php
+++ trunk/ThinkPHP/Common/extend.php
@@ -427,4 +427,117 @@ function RemoveXSS($val) {
    }
    return $val;
 }
+
+/**
+ +----------------------------------------------------------
+ * 把返回的数据集转换成Tree
+ +----------------------------------------------------------
+ * @access public
+ +----------------------------------------------------------
+ * @param array $list 要转换的数据集
+ * @param string $pid parent标记字段
+ * @param string $level level标记字段
+ +----------------------------------------------------------
+ * @return array
+ +----------------------------------------------------------
+ */
+function list_to_tree($list, $pk='id',$pid = 'pid',$child = '_child',$root=0)
+{
+    // 创建Tree
+    $tree = array();
+    if(is_array($list)) {
+        // 创建基于主键的数组引用
+        $refer = array();
+        foreach ($list as $key => $data) {
+            $refer[$data[$pk]] =& $list[$key];
+        }
+        foreach ($list as $key => $data) {
+            // 判断是否存在parent
+            $parentId = $data[$pid];
+            if ($root == $parentId) {
+                $tree[] =& $list[$key];
+            }else{
+                if (isset($refer[$parentId])) {
+                    $parent =& $refer[$parentId];
+                    $parent[$child][] =& $list[$key];
+                }
+            }
+        }
+    }
+    return $tree;
+}
+
+/**
+ +----------------------------------------------------------
+ * 对查询结果集进行排序
+ +----------------------------------------------------------
+ * @access public
+ +----------------------------------------------------------
+ * @param array $list 查询结果
+ * @param string $field 排序的字段名
+ * @param array $sortby 排序类型 asc arsort natcaseror
+ +----------------------------------------------------------
+ * @return array
+ +----------------------------------------------------------
+ */
+function list_sort_by($list,$field, $sortby='asc') {
+   if(is_array($list)){
+       $refer = $resultSet = array();
+       foreach ($list as $i => $data)
+           $refer[$i] = &$data[$field];
+       switch ($sortby) {
+           case 'asc': // 正向排序
+                asort($refer);
+                break;
+           case 'desc':// 逆向排序
+                arsort($refer);
+                break;
+           case 'nat': // 自然排序
+                natcasesort($refer);
+                break;
+       }
+       foreach ( $refer as $key=> $val)
+           $resultSet[] = &$list[$key];
+       return $resultSet;
+   }
+   return false;
+}
+
+/**
+ +----------------------------------------------------------
+ * 在数据列表中搜索
+ +----------------------------------------------------------
+ * @access public
+ +----------------------------------------------------------
+ * @param array $list 数据列表
+ * @param mixed $condition 查询条件
+ * 支持 array('name'=>$value) 或者 name=$value
+ +----------------------------------------------------------
+ * @return array
+ +----------------------------------------------------------
+ */
+function search_list($list,$condition) {
+    if(null === $list)
+        // 默认直接在查询返回的结果集中搜索
+        $list   =   &$this->dataList;
+    if(is_string($condition))
+        parse_str($condition,$condition);
+    // 返回的结果集合
+    $resultSet = array();
+    foreach ($list as $key=>$data){
+        $find   =   false;
+        foreach ($condition as $field=>$value){
+            if(isset($data[$field])) {
+                if(0 === strpos($value,'/')) {
+                    $find   =   preg_match($value,$data[$field]);
+                }elseif($data[$field]==$value){
+                    $find = true;
+                }
+            }
+        }
+        if($find)
+            $resultSet[]     =   &$list[$key];
+    }
+    return $resultSet;
+}
 ?>
