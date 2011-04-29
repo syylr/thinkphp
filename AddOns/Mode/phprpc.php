eae7a4a4c918592ae63a5ae0651db352469ec6da
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-04-29 12:21:32 +0000

    完善Phprpc模式扩展的模型自动加载

--- AddOns/Mode/phprpc.php
+++ AddOns/Mode/phprpc.php
@@ -18,6 +18,7 @@ return array(
     THINK_PATH.'/Lib/Think/Core/Log.class.php',// 日志处理
     THINK_PATH.'/Mode/Phprpc/App.class.php', // 应用程序类
     THINK_PATH.'/Mode/Phprpc/Action.class.php',// 控制器类
+    THINK_PATH.'/Lib/Think/Core/Model.class.php', // 模型类
     THINK_PATH.'/Mode/Phprpc/alias.php', // 加载别名
 );
 ?>
