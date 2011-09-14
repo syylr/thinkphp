f61d95a90d1dfc371cb9e18c1e0acab5cc66857e
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-09-14 02:14:15 +0000

    mysql和mysqli驱动insertAll方法增加replace参数

--- ThinkPHP/Lib/Think/Db/Driver/DbMysql.class.php
+++ ThinkPHP/Lib/Think/Db/Driver/DbMysql.class.php
@@ -329,11 +329,12 @@ class DbMysql extends Db{
      +----------------------------------------------------------
      * @param mixed $datas 数据
      * @param array $options 参数表达式
+     * @param boolean $replace 是否replace
      +----------------------------------------------------------
      * @return false | integer
      +----------------------------------------------------------
      */
-    public function insertAll($datas,$options=array()) {
+    public function insertAll($datas,$options=array(),$replace=false) {
         if(!is_array($datas[0])) return false;
         $fields = array_keys($datas[0]);
         array_walk($fields, array($this, 'addSpecialChar'));
@@ -348,7 +349,7 @@ class DbMysql extends Db{
             }
             $values[]    = '('.implode(',', $value).')';
         }
-        $sql   =  'INSERT INTO '.$this->parseTable($options['table']).' ('.implode(',', $fields).') VALUES '.implode(',',$values);
+        $sql   =  ($replace?'REPLACE':'INSERT').' INTO '.$this->parseTable($options['table']).' ('.implode(',', $fields).') VALUES '.implode(',',$values);
         return $this->execute($sql);
     }
 
