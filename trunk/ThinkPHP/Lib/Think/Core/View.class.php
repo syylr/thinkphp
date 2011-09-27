34626806b949171a4f9352cb8f3dcd539da3dd80
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-09-27 03:11:47 +0000

    改进模板输出替换变量__GROUP__ 如果当前没有分组则输出同 __APP__

--- trunk/ThinkPHP/Lib/Think/Core/View.class.php
+++ trunk/ThinkPHP/Lib/Think/Core/View.class.php
@@ -325,16 +325,13 @@ class View extends Think
             '__TMPL__'      => APP_TMPL_PATH,  // 项目模板目录
             '__ROOT__'      => __ROOT__,       // 当前网站地址
             '__APP__'       => __APP__,        // 当前项目地址
+            '__GROUP__'   =>   defined('GROUP_NAME')?__GROUP__:__APP__,
             '__UPLOAD__'    => __ROOT__.'/Uploads',
             '__ACTION__'    => __ACTION__,     // 当前操作地址
             '__SELF__'      => __SELF__,       // 当前页面地址
             '__URL__'       => __URL__,
             '__INFO__'      => __INFO__,
         );
-        if(defined('GROUP_NAME'))
-        {
-            $replace['__GROUP__'] = __GROUP__;// 当前项目地址
-        }
         if(C('TOKEN_ON')) {
             if(strpos($content,'{__TOKEN__}')) {
                 // 指定表单令牌隐藏域位置
