711a5a772ec540aaf4bcc3b41e15c7e67f5b0385
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-10-15 02:21:17 +0000

    重写get_client_ip函数 修正iis下面的问题， 优化而且增加合法性验证，并且从扩展函数库移到内置函数库

--- ThinkPHP/Common/extend.php
+++ ThinkPHP/Common/extend.php
@@ -21,21 +21,6 @@
  +------------------------------------------------------------------------------
  */
 
-// 获取客户端IP地址
-function get_client_ip(){
-   if (getenv("HTTP_CLIENT_IP") && strcasecmp(getenv("HTTP_CLIENT_IP"), "unknown"))
-       $ip = getenv("HTTP_CLIENT_IP");
-   else if (getenv("HTTP_X_FORWARDED_FOR") && strcasecmp(getenv("HTTP_X_FORWARDED_FOR"), "unknown"))
-       $ip = getenv("HTTP_X_FORWARDED_FOR");
-   else if (getenv("REMOTE_ADDR") && strcasecmp(getenv("REMOTE_ADDR"), "unknown"))
-       $ip = getenv("REMOTE_ADDR");
-   else if (isset($_SERVER['REMOTE_ADDR']) && $_SERVER['REMOTE_ADDR'] && strcasecmp($_SERVER['REMOTE_ADDR'], "unknown"))
-       $ip = $_SERVER['REMOTE_ADDR'];
-   else
-       $ip = "unknown";
-   return($ip);
-}
-
 /**
  +----------------------------------------------------------
  * 字符串截取，支持中文和其他编码
