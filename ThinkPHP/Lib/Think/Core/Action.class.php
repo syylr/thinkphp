1d9ddf48fcc148cc2558a3a458f91f8a5e414b6c
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-10-16 03:06:42 +0000

    修正Action类的get方法的冲突问题

--- ThinkPHP/Lib/Think/Core/Action.class.php
+++ ThinkPHP/Lib/Think/Core/Action.class.php
@@ -176,11 +176,6 @@ abstract class Action extends Think
      * @return mixed
      +----------------------------------------------------------
      */
-    protected function get($name)
-    {
-        return $this->view->get($name);
-    }
-
     public function __get($name) {
         return $this->view->get($name);
     }
@@ -393,22 +388,22 @@ abstract class Action extends Think
         // 提示标题
         $this->assign('msgTitle',$status? L('_OPERATION_SUCCESS_') : L('_OPERATION_FAIL_'));
         //如果设置了关闭窗口，则提示完毕后自动关闭窗口
-        if($this->get('closeWin'))    $this->assign('jumpUrl','javascript:window.close();');
+        if($this->view->get('closeWin'))    $this->assign('jumpUrl','javascript:window.close();');
         $this->assign('status',$status);   // 状态
         $this->assign('message',$message);// 提示信息
         //保证输出不受静态缓存影响
         C('HTML_CACHE_ON',false);
         if($status) { //发送成功信息
             // 成功操作后默认停留1秒
-            if(!$this->get('waitSecond'))    $this->assign('waitSecond',"1");
+            if(!$this->view->get('waitSecond'))    $this->assign('waitSecond',"1");
             // 默认操作成功自动返回操作前页面
-            if(!$this->get('jumpUrl')) $this->assign("jumpUrl",$_SERVER["HTTP_REFERER"]);
+            if(!$this->view->get('jumpUrl')) $this->assign("jumpUrl",$_SERVER["HTTP_REFERER"]);
             $this->display(C('TMPL_ACTION_SUCCESS'));
         }else{
             //发生错误时候默认停留3秒
-            if(!$this->get('waitSecond'))    $this->assign('waitSecond',"3");
+            if(!$this->view->get('waitSecond'))    $this->assign('waitSecond',"3");
             // 默认发生错误的话自动返回上页
-            if(!$this->get('jumpUrl')) $this->assign('jumpUrl',"javascript:history.back(-1);");
+            if(!$this->view->get('jumpUrl')) $this->assign('jumpUrl',"javascript:history.back(-1);");
             $this->display(C('TMPL_ACTION_ERROR'));
         }
         if(C('LOG_RECORD')) Log::save();
