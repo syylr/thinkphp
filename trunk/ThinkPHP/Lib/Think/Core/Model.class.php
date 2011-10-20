477e0a94182b2b6f76ec76cee62cf91c4b346074
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-10-20 07:43:58 +0000

    Model增加check方法 用于个别需要的情况手动验证数据,支持部分自动验证的规则 用法 check('验证数据'，'验证规则','验证类型') 验证类型支持 in between equal length regex expire ip_allow ip_deny，默认为regex 结果返回布尔值 $model->check($value,'email'); $model->check($value,'1,2,3','in');

--- trunk/ThinkPHP/Lib/Think/Core/Model.class.php
+++ trunk/ThinkPHP/Lib/Think/Core/Model.class.php
@@ -1009,14 +1009,6 @@ class Model extends Think
                 }
             case 'confirm': // 验证两个字段是否相同
                 return $data[$val[0]] == $data[$val[1]];
-            case 'in': // 验证是否在某个指定范围之内 逗号分隔字符串或者数组
-                $range   = is_array($val[1])?$val[1]:explode(',',$val[1]);
-                return in_array($data[$val[0]] ,$range);
-            case 'between': // 验证是否在某个范围
-                list($min,$max)   =  explode(',',$val[1]);
-                return $data[$val[0]]>=$min && $data[$val[0]]<=$max;
-            case 'equal': // 验证是否等于某个值
-                return $data[$val[0]] == $val[1];
             case 'unique': // 验证某个值是否唯一
                 if(is_string($val[0]) && strpos($val[0],','))
                     $val[0]  =  explode(',',$val[0]);
@@ -1031,32 +1023,58 @@ class Model extends Think
                 if(!empty($data[$this->getPk()])) { // 完善编辑的时候验证唯一
                     $map[$this->getPk()] = array('neq',$data[$this->getPk()]);
                 }
-                if($this->where($map)->find())
-                    return false;
-                break;
+                if($this->where($map)->find())   return false;
+                return true;
+            default:  // 检查附加规则
+                return $this->validate($data[$val[0]],$val[1],$val[4]);
+        }
+    }
+
+    /**
+     +----------------------------------------------------------
+     * 验证数据 支持 in between equal length regex expire ip_allow ip_deny
+     +----------------------------------------------------------
+     * @access public
+     +----------------------------------------------------------
+     * @param string $value 验证数据
+     * @param mixed $rule 验证表达式
+     * @param string $type 验证方式 默认为正则验证
+     +----------------------------------------------------------
+     * @return boolean
+     +----------------------------------------------------------
+     */
+    public function check($value,$rule,$type='regex'){
+        switch(strtolower($type)) {
+            case 'in': // 验证是否在某个指定范围之内 逗号分隔字符串或者数组
+                $range   = is_array($rule)?$rule:explode(',',$rule);
+                return in_array($value ,$range);
+            case 'between': // 验证是否在某个范围
+                list($min,$max)   =  explode(',',$rule);
+                return $value>=$min && $value<=$max;
+            case 'equal': // 验证是否等于某个值
+                return $value == $rule;
             case 'length': // 验证长度
-                $length  =  mb_strlen($data[$val[0]],'utf-8'); // 当前数据长度
-                if(strpos($val[1],',')) { // 长度区间
-                    list($min,$max)   =  explode(',',$val[1]);
+                $length  =  mb_strlen($value,'utf-8'); // 当前数据长度
+                if(strpos($rule,',')) { // 长度区间
+                    list($min,$max)   =  explode(',',$rule);
                     return $length >= $min && $length <= $max;
                 }else{// 指定长度
-                    return $length == $val[1];
+                    return $length == $rule;
                 }
-            case 'expire': // 操作有效期验证 支持时间戳和日期格式定义
-                list($start,$end)   =  explode(',',$val[1]);
+            case 'expire':
+                list($start,$end)   =  explode(',',$rule);
                 if(!is_numeric($start)) $start   =  strtotime($start);
                 if(!is_numeric($end)) $end   =  strtotime($end);
                 return NOW_TIME >= $start && NOW_TIME <= $end;
             case 'ip_allow': // IP 操作许可验证
-                return in_array(get_client_ip(),explode(',',$val[1]));
+                return in_array(get_client_ip(),explode(',',$rule));
             case 'ip_deny': // IP 操作禁止验证
-                return !in_array(get_client_ip(),explode(',',$val[1]));
+                return !in_array(get_client_ip(),explode(',',$rule));
             case 'regex':
             default:    // 默认使用正则验证 可以使用验证类中定义的验证名称
                 // 检查附加规则
-                return $this->regex($data[$val[0]],$val[1]);
+                return $this->regex($value,$rule);
         }
-        return true;
     }
 
     /**
