0a4787409d30eab1020ebddcb07a7ca91a569ad3
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-11-02 09:39:44 +0000

    调试模式配置参数调整

--- trunk/ThinkPHP/Common/convention.php
+++ trunk/ThinkPHP/Common/convention.php
@@ -118,7 +118,7 @@ return  array(
     'SHOW_CACHE_TIMES'		=> false,   // 显示缓存操作次数
     'SHOW_USE_MEM'			=> false,   // 显示内存开销
     'SHOW_PAGE_TRACE'		=> false,   // 显示页面Trace信息 由Trace文件定义和Action操作赋值
-    'SHOW_ERROR_MSG'        => true,    // 显示错误信息
+    'SHOW_ERROR_MSG'        => false,    // 显示错误信息
 
     /* 模板引擎设置 */
     'TMPL_ENGINE_TYPE'		=> 'Think',     // 默认模板引擎 以下设置仅对使用Think模板引擎有效
@@ -132,7 +132,7 @@ return  array(
     'TMPL_L_DELIM'          => '{',			// 模板引擎普通标签开始标记
     'TMPL_R_DELIM'          => '}',			// 模板引擎普通标签结束标记
     'TMPL_VAR_IDENTIFY'     => 'array',     // 模板变量识别。留空自动判断,参数为'obj'则表示对象
-    'TMPL_STRIP_SPACE'      => false,       // 是否去除模板文件里面的html空格与换行
+    'TMPL_STRIP_SPACE'      => true,       // 是否去除模板文件里面的html空格与换行
     'TMPL_CACHE_ON'			=> true,        // 是否开启模板编译缓存,设为false则每次都会重新编译
     'TMPL_CACHE_TIME'		=>	-1,         // 模板缓存有效期 -1 为永久，(以数字为值，单位:秒)
     'TMPL_ACTION_ERROR'     => 'Public:success', // 默认错误跳转对应的模板文件
@@ -140,6 +140,7 @@ return  array(
     'TMPL_TRACE_FILE'       => THINK_PATH.'/Tpl/PageTrace.tpl.php',     // 页面Trace的模板文件
     'TMPL_EXCEPTION_FILE'   => THINK_PATH.'/Tpl/ThinkException.tpl.php',// 异常页面的模板文件
     'TMPL_FILE_DEPR'=>'/', //模板文件MODULE_NAME与ACTION_NAME之间的分割符，只对项目分组部署有效
+
     // Think模板引擎标签库相关设定
     'TAGLIB_BEGIN'          => '<',  // 标签库标签开始标记
     'TAGLIB_END'            => '>',  // 标签库标签结束标记
