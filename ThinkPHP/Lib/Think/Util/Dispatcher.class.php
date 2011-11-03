26e21368f7bbf5d50c45ff02322e46a808265967
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-11-03 03:34:11 +0000

    把__SELF__常量定义放回Dispatcher 避免被编译缓存固化

--- ThinkPHP/Lib/Think/Util/Dispatcher.class.php
+++ ThinkPHP/Lib/Think/Util/Dispatcher.class.php
@@ -115,6 +115,7 @@ class Dispatcher extends Think
         define('MODULE_NAME',self::getModule(C('VAR_MODULE')));
         define('ACTION_NAME',self::getAction(C('VAR_ACTION')));
         // URL常量
+        define('__SELF__',$_SERVER['REQUEST_URI']);
         define('__INFO__',$_SERVER['PATH_INFO']);
         // 当前项目地址
         define('__APP__',PHP_FILE);
