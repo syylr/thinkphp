f81161b030acd20e99ca4fb996cfcd08a23f62a4
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-11-21 04:37:08 +0000

    修正 strip_whitespace 函数对heredoc字符串的支持

--- trunk/ThinkPHP/Common/functions.php
+++ trunk/ThinkPHP/Common/functions.php
@@ -749,6 +749,20 @@ function strip_whitespace($content) {
                         $last_space = true;
                     }
                     break;
+                case T_START_HEREDOC:
+                    $stripStr .= "<<<THINK\n";
+                    break;
+                case T_END_HEREDOC:
+                    $stripStr .= "THINK;\n";
+                    for($k = $i+1; $k < $j; $k++) {
+                        if(is_string($tokens[$k]) && $tokens[$k] == ";") {
+                            $i = $k;
+                            break;
+                        } else if($tokens[$k][0] == T_CLOSE_TAG) {
+                            break;
+                        }
+                    }
+                    break;
                 default:
                     $last_space = false;
                     $stripStr .= $tokens[$i][1];
