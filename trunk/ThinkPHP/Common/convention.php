f9e1be9b315e9a01b2bc075b39b25e68bee8f9b5
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-11-29 07:58:16 +0000

    惯例配置添加 APP_SUB_DOMAIN_RULES和APP_SUB_DOMAIN_DENY 默认设置

--- trunk/ThinkPHP/Common/convention.php
+++ trunk/ThinkPHP/Common/convention.php
@@ -27,7 +27,9 @@ if (!defined('THINK_PATH')) exit();
 return  array(
 
     /* 项目设定 */
-    'APP_SUB_DOMAIN_DEPLOY' => false,   // 是否开启子域名
+    'APP_SUB_DOMAIN_DEPLOY' => false,   // 是否开启子域名部署
+    'APP_SUB_DOMAIN_RULES'=>array(), // 子域名部署规则
+    'APP_SUB_DOMAIN_DENY'=>array(), //  子域名禁用列表
     'APP_FILE_CASE'         => false,   // 是否检查文件的大小写 对Windows平台有效
     'APP_GROUP_DEPR'        => '.',     // 模块分组之间的分割符
     'APP_GROUP_LIST'        => '',      // 项目分组设定,多个组之间用逗号分隔,例如'Home,Admin'
