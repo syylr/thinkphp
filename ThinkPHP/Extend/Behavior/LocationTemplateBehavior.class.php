602c25e3124d57ac5b3042f76f91bb99ff305db1
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-11-30 14:59:53 +0000

    修改view_begin标签 增加templateFile参数 增加view_parse标签 并增加LocationTemplate和ParseTemplate行为扩展

---
+++ ThinkPHP/Extend/Behavior/LocationTemplateBehavior.class.php
@@ -0,0 +1,56 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2010 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+// $Id$
+
+/**
+ +------------------------------------------------------------------------------
+ * 系统行为扩展 自动定位模板文件
+ * 增加配置参数如下：
+ +------------------------------------------------------------------------------
+ */
+class LocationTemplateBehavior {
+    // 行为扩展的执行入口必须是run
+    public function run(&$templateFile){
+        // 自动定位模板文件
+        if(!file_exists_case($templateFile))
+            $templateFile   = $this->parseTemplateFile($templateFile);
+    }
+
+    /**
+     +----------------------------------------------------------
+     * 自动定位模板文件
+     +----------------------------------------------------------
+     * @access private
+     +----------------------------------------------------------
+     * @param string $templateFile 文件名
+     +----------------------------------------------------------
+     * @return string
+     +----------------------------------------------------------
+     * @throws ThinkExecption
+     +----------------------------------------------------------
+     */
+    private function parseTemplateFile($templateFile) {
+        if(''==$templateFile) {
+            // 如果模板文件名为空 按照默认规则定位
+            $templateFile = C('TMPL_FILE_NAME');
+        }elseif(false === strpos($templateFile,'.')){
+            $templateFile  = str_replace(array('@',':'),'/',$templateFile);
+            $count   =  substr_count($templateFile,'/');
+            $path   = dirname(C('TMPL_FILE_NAME'));
+            for($i=0;$i<$count;$i++)
+                $path   = dirname($path);
+            $templateFile =  $path.'/'.$templateFile.C('TMPL_TEMPLATE_SUFFIX');
+        }
+        if(!file_exists_case($templateFile))
+            throw_exception(L('_TEMPLATE_NOT_EXIST_').'['.$templateFile.']');
+        return $templateFile;
+    }
+}
