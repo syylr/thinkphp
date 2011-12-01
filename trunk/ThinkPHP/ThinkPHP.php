2954c0ef752b179ff530cb199bd2ae7f4a959e97
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-12-01 13:38:48 +0000

    修正init运行时间的显示问题

--- trunk/ThinkPHP/ThinkPHP.php
+++ trunk/ThinkPHP/ThinkPHP.php
@@ -35,11 +35,11 @@ if(!APP_DEBUG && is_file(RUNTIME_PATH.$runtime)) {
     // 加载运行时文件
     require THINK_PATH."Common/runtime.php";
 }
+// 记录加载文件时间
+G('loadTime');
 // 执行应用
 App::run();
 
-// 记录加载文件时间
-G('loadTime');
 // 记录和统计时间（微秒）
 function G($start,$end='',$dec=3) {
     static $_info = array();
