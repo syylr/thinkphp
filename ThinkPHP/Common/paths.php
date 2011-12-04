55d203c41baa243042e0c61908d6e2ebb7b545f6
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-12-04 05:14:04 +0000

    基类库目录移动到Extend/Library 核心Lib目录更名为Core

--- ThinkPHP/Common/paths.php
+++ ThinkPHP/Common/paths.php
@@ -30,9 +30,11 @@ define('LOG_PATH',       RUNTIME_PATH.LOG_DIR.'/'); //
 define('LANG_PATH',     APP_PATH.LANG_DIR.'/'); //
 define('TEMP_PATH',      RUNTIME_PATH.TEMP_DIR.'/'); //
 define('DATA_PATH', RUNTIME_PATH.'Data/'); //
+define('CORE_PATH',THINK_PATH.'Core/');
 define('EXTEND_PATH',THINK_PATH.'Extend/');
 define('MODE_PATH',EXTEND_PATH.'Mode/');
 define('VENDOR_PATH',EXTEND_PATH.'Vendor/');
+define('LIBRARY_PATH',EXTEND_PATH.'Library/');
 //[/RUNTIME]
 // 为了方便导入第三方类库 设置Vendor目录到include_path
 set_include_path(get_include_path() . PATH_SEPARATOR . VENDOR_PATH);
