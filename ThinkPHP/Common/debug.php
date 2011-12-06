09815cc3d964f891262d875f5fb92e1191e6c16d
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-12-06 05:29:23 +0000

    默认调试模式配置调整 默认情况下不开启运行信息和页面Trace信息

--- ThinkPHP/Common/debug.php
+++ ThinkPHP/Common/debug.php
@@ -23,20 +23,9 @@
  */
 if (!defined('THINK_PATH')) exit();
 return  array(
-    /* 日志设置 */
     'LOG_RECORD'=>true,  // 进行日志记录
-
-    /* 数据库设置 */
     'LOG_RECORD_LEVEL'       =>   'EMERG,ALERT,CRIT,ERR,WARN,NOTIC,INFO,DEBUG,SQL',  // 允许记录的日志级别
-    'DB_FIELDS_CACHE'=> false,
-
-    /* 运行时间设置 */
-    'SHOW_RUN_TIME'=>true,          // 运行时间显示
-    'SHOW_ADV_TIME'=>true,          // 显示详细的运行时间
-    'SHOW_DB_TIMES'=>true,          // 显示数据库查询和写入次数
-    'SHOW_CACHE_TIMES'=>true,       // 显示缓存操作次数
-    'SHOW_USE_MEM'=>true,           // 显示内存开销
-    'SHOW_PAGE_TRACE'=>true,        // 显示页面Trace信息 由Trace文件定义和Action操作赋值
+    'DB_FIELDS_CACHE'=> false, // 字段缓存信息
     'APP_FILE_CASE'  =>   true, // 是否检查文件的大小写 对Windows平台有效
     'TMPL_STRIP_SPACE'      => false,       // 是否去除模板文件里面的html空格与换行
     'SHOW_ERROR_MSG'        => true,    // 显示错误信息
