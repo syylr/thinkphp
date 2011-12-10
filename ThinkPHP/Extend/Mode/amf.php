b68c019d1919b6e4a850ae7190dc67e7b23fd4d3
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-12-10 12:47:12 +0000

    模式扩展改造 废除Extend/Collection目录

--- ThinkPHP/Extend/Mode/amf.php
+++ ThinkPHP/Extend/Mode/amf.php
@@ -10,13 +10,31 @@
 // +----------------------------------------------------------------------
 // $Id$
 
-// 简洁模式核心定义文件列表
+// AMF模式定义文件
 return array(
-    THINK_PATH.'Common/functions.php',   // 系统函数库
-    CORE_PATH.'Core/Think.class.php',
-    CORE_PATH.'Core/ThinkException.class.php',// 异常处理
-    CORE_PATH.'Core/Log.class.php',// 日志处理
-    MODE_PATH.'Amf/App.class.php', // 应用程序类
-    MODE_PATH.'Amf/Action.class.php',// 控制器类
+    'core'         =>   array(
+        THINK_PATH.'Common/functions.php',   // 系统函数库
+        CORE_PATH.'Core/Log.class.php',// 日志处理
+        MODE_PATH.'Amf/App.class.php', // 应用程序类
+        MODE_PATH.'Amf/Action.class.php',// 控制器类
+    ),
+
+    // 项目别名定义文件 [支持数组直接定义或者文件名定义]
+    'alias'         =>    array(
+        'Model'         =>   MODE_PATH.'Amf/Model.class.php',
+        'Db'                  =>    MODE_PATH.'Amf/Db.class.php',
+    ), 
+
+    // 系统行为定义文件 [必须 支持数组直接定义或者文件名定义 ]
+    'extends'    =>    array(), 
+
+    // 项目应用行为定义文件 [支持数组直接定义或者文件名定义]
+    'tags'         =>   array(), 
+
+    // 项目公共文件
+    'common'   =>    COMMON_PATH.'common.php', 
+
+    // 模式配置文件  [支持数组直接定义或者文件名定义]（如有相同则覆盖项目配置文件中的配置）
+    'config'       =>   array(), 
 );
 ?>
--- ThinkPHP/Extend/Mode/rest.php
+++ ThinkPHP/Extend/Mode/rest.php
@@ -12,13 +12,27 @@
 
 // REST模式核心定义文件列表
 return array(
-    THINK_PATH.'Common/functions.php',   // 系统函数库
-    CORE_PATH.'Core/Think.class.php',
-    CORE_PATH.'Core/ThinkException.class.php',// 异常处理
-    CORE_PATH.'Core/Log.class.php',// 日志处理
-    CORE_PATH.'Core/App.class.php', // 应用程序类
-    MODE_PATH.'Rest/Dispatcher.class.php',// URL路由
-    MODE_PATH.'Rest/Action.class.php',// 控制器类
-    MODE_PATH.'Rest/common.php', // 加载REST公共函数
+
+    'core'         =>   array(
+        THINK_PATH.'Common/functions.php',   // 系统函数库
+        CORE_PATH.'Core/Log.class.php',// 日志处理
+        MODE_PATH.'Core/App.class.php', // 应用程序类
+        MODE_PATH.'Rest/Action.class.php',// 控制器类
+    ),
+
+    // 项目别名定义文件 [支持数组直接定义或者文件名定义]
+    'alias'         =>    array(), 
+
+    // 系统行为定义文件 [必须 支持数组直接定义或者文件名定义 ]
+    'extends'    =>    MODE_PATH.'Rest/tags.php',
+
+    // 项目应用行为定义文件 [支持数组直接定义或者文件名定义]
+    'tags'         =>   array(), 
+
+    // 项目公共文件
+    'common'   =>    COMMON_PATH.'common.php', 
+
+    // 模式配置文件  [支持数组直接定义或者文件名定义]（如有相同则覆盖项目配置文件中的配置）
+    'config'   =>   MODE_PATH.'Rest/config.php',
 );
 ?>
