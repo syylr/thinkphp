53e436b2c9849222f8945c5889da9b7be776cdfd
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-12-11 10:38:30 +0000

    减少路径常量

--- trunk/ThinkPHP/Lib/Behavior/CheckTemplateBehavior.class.php
+++ trunk/ThinkPHP/Lib/Behavior/CheckTemplateBehavior.class.php
@@ -59,11 +59,11 @@ class CheckTemplateBehavior extends Behavior {
         /* 模板相关目录常量 */
         define('THEME_NAME',   $templateSet);                  // 当前模板主题名称
         if(defined('GROUP_NAME')) {
-            define('APP_TMPL_PATH',   __ROOT__.'/'.APP_NAME.(APP_NAME?'/':'').TMPL_DIR.'/'.GROUP_NAME.(THEME_NAME?'/':'').THEME_NAME.'/');
+            define('APP_TMPL_PATH',   __ROOT__.'/'.APP_NAME.(APP_NAME?'/':'').'Tpl/'.GROUP_NAME.(THEME_NAME?'/':'').THEME_NAME.'/');
             C('TEMPLATE_NAME',TMPL_PATH.GROUP_NAME.(THEME_NAME?'/':'').THEME_NAME.'/'.MODULE_NAME.C('TMPL_FILE_DEPR').ACTION_NAME.C('TMPL_TEMPLATE_SUFFIX'));
             C('CACHE_PATH',CACHE_PATH.GROUP_NAME.'/');
         }else{
-            define('APP_TMPL_PATH',   __ROOT__.'/'.APP_NAME.(APP_NAME?'/':'').TMPL_DIR.'/'.THEME_NAME.(THEME_NAME?'/':''));
+            define('APP_TMPL_PATH',   __ROOT__.'/'.APP_NAME.(APP_NAME?'/':'').'Tpl/'.THEME_NAME.(THEME_NAME?'/':''));
             C('TEMPLATE_NAME',TMPL_PATH.THEME_NAME.(THEME_NAME?'/':'').MODULE_NAME.'/'.ACTION_NAME.C('TMPL_TEMPLATE_SUFFIX'));
             C('CACHE_PATH',CACHE_PATH);
         }
