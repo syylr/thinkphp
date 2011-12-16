877a94edab8d236629e413abd85661dea1023350
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-12-16 05:43:12 +0000

    修正系统别名的加载和编译 增加别名定义文件

---
+++ trunk/ThinkPHP/Conf/alias.php
@@ -0,0 +1,24 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2010 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+// $Id$
+
+// 系统别名定义文件
+return array(
+    'Model'         => CORE_PATH.'Core/Model.class.php',
+    'Db'            => CORE_PATH.'Core/Db.class.php',
+    'Log'          =>   CORE_PATH.'Core/Log.class.php',
+    'ThinkTemplate' => CORE_PATH.'Template/ThinkTemplate.class.php',
+    'TagLib'        => CORE_PATH.'Template/TagLib.class.php',
+    'Cache'         => CORE_PATH.'Core/Cache.class.php',
+    'Debug'         => CORE_PATH.'Util/Debug.class.php',
+    'Session'       => CORE_PATH.'Util/Session.class.php',
+    'TagLibCx'      => CORE_PATH.'Driver/TagLib/TagLibCx.class.php',
+);
