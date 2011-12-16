955ffa9b5ca8bde33a4dacef25f287a84b63e9b0
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-12-16 09:24:31 +0000

    布局标签解析修正

--- trunk/ThinkPHP/Lib/Template/ThinkTemplate.class.php
+++ trunk/ThinkPHP/Lib/Template/ThinkTemplate.class.php
@@ -246,9 +246,8 @@ class  ThinkTemplate extends Think
                 throw_exception(L('_XML_TAG_ERROR_'));
             $xml = (array)($xml->tag->attributes());
             $array = array_change_key_case($xml['@attributes']);
-            $layoutName = explode(',',$array['name']);
             // 读取布局模板
-            $layoutFile  =  THEME_PATH.$layoutName.$this->config['template_suffix'];
+            $layoutFile  =  THEME_PATH.$array['name'].$this->config['template_suffix'];
             // 替换布局的主体内容
             $content = str_replace('{__CONTENT__}',$content,file_get_contents($layoutFile));
         }
