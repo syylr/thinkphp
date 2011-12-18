98ea3a8d973468e7b8e40c4ff88695f190a88165
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-12-18 05:21:45 +0000

    cli模式采用标准版的model和db类 取消个别命令行下面不需使用的函数

--- trunk/ThinkPHP/Extend/Mode/Cli/functions.php
+++ trunk/ThinkPHP/Extend/Mode/Cli/functions.php
@@ -167,92 +167,6 @@ function to_guid_string($mix) {
     return md5($mix);
 }
 
-// xml编码
-function xml_encode($data, $encoding='utf-8', $root="think") {
-    $xml = '<?xml version="1.0" encoding="' . $encoding . '"?>';
-    $xml.= '<' . $root . '>';
-    $xml.= data_to_xml($data);
-    $xml.= '</' . $root . '>';
-    return $xml;
-}
-
-function data_to_xml($data) {
-    if (is_object($data)) {
-        $data = get_object_vars($data);
-    }
-    $xml = '';
-    foreach ($data as $key => $val) {
-        is_numeric($key) && $key = "item id=\"$key\"";
-        $xml.="<$key>";
-        $xml.= ( is_array($val) || is_object($val)) ? data_to_xml($val) : $val;
-        list($key, ) = explode(' ', $key);
-        $xml.="</$key>";
-    }
-    return $xml;
-}
-
-/**
-  +----------------------------------------------------------
- * Cookie 设置、获取、清除
-  +----------------------------------------------------------
- * 1 获取cookie: cookie('name')
- * 2 清空当前设置前缀的所有cookie: cookie(null)
- * 3 删除指定前缀所有cookie: cookie(null,'think_') | 注：前缀将不区分大小写
- * 4 设置cookie: cookie('name','value') | 指定保存时间: cookie('name','value',3600)
- * 5 删除cookie: cookie('name',null)
-  +----------------------------------------------------------
- * $option 可用设置prefix,expire,path,domain
- * 支持数组形式对参数设置:cookie('name','value',array('expire'=>1,'prefix'=>'think_'))
- * 支持query形式字符串对参数设置:cookie('name','value','prefix=tp_&expire=10000')
- */
-function cookie($name, $value='', $option=null) {
-    // 默认设置
-    $config = array(
-        'prefix' => C('COOKIE_PREFIX'), // cookie 名称前缀
-        'expire' => C('COOKIE_EXPIRE'), // cookie 保存时间
-        'path' => C('COOKIE_PATH'), // cookie 保存路径
-        'domain' => C('COOKIE_DOMAIN'), // cookie 有效域名
-    );
-    // 参数设置(会覆盖黙认设置)
-    if (!empty($option)) {
-        if (is_numeric($option))
-            $option = array('expire' => $option);
-        elseif (is_string($option))
-            parse_str($option, $option);
-        $config = array_merge($config, array_change_key_case($option));
-    }
-    // 清除指定前缀的所有cookie
-    if (is_null($name)) {
-        if (empty($_COOKIE))
-            return;
-        // 要删除的cookie前缀，不指定则删除config设置的指定前缀
-        $prefix = empty($value) ? $config['prefix'] : $value;
-        if (!empty($prefix)) {// 如果前缀为空字符串将不作处理直接返回
-            foreach ($_COOKIE as $key => $val) {
-                if (0 === stripos($key, $prefix)) {
-                    setcookie($key, '', time() - 3600, $config['path'], $config['domain']);
-                    unset($_COOKIE[$key]);
-                }
-            }
-        }
-        return;
-    }
-    $name = $config['prefix'] . $name;
-    if ('' === $value) {
-        return isset($_COOKIE[$name]) ? $_COOKIE[$name] : null; // 获取指定Cookie
-    } else {
-        if (is_null($value)) {
-            setcookie($name, '', time() - 3600, $config['path'], $config['domain']);
-            unset($_COOKIE[$name]); // 删除指定cookie
-        } else {
-            // 设置cookie
-            $expire = !empty($config['expire']) ? time() + intval($config['expire']) : 0;
-            setcookie($name, $value, $expire, $config['path'], $config['domain']);
-            $_COOKIE[$name] = $value;
-        }
-    }
-}
-
 // 加载扩展配置文件
 function load_ext_file() {
     // 加载自定义外部文件
@@ -274,23 +188,4 @@ function load_ext_file() {
             }
         }
     }
-}
-
-// 获取客户端IP地址
-function get_client_ip() {
-    static $ip = NULL;
-    if ($ip !== NULL) return $ip;
-    if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        $arr = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-        $pos =  array_search('unknown',$arr);
-        if(false !== $pos) unset($arr[$pos]);
-        $ip   =  trim($arr[0]);
-    }elseif (isset($_SERVER['HTTP_CLIENT_IP'])) {
-        $ip = $_SERVER['HTTP_CLIENT_IP'];
-    }elseif (isset($_SERVER['REMOTE_ADDR'])) {
-        $ip = $_SERVER['REMOTE_ADDR'];
-    }
-    // IP地址合法验证
-    $ip = (false !== ip2long($ip)) ? $ip : '0.0.0.0';
-    return $ip;
 }
