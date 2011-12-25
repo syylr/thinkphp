647ac4edbabffc808ba7d1a7997f1e7d4fb3222b
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2011-12-25 08:46:39 +0000

    模板输出规则调整为 模板主题:模块:操作 不支持 跨项目和跨分组调用
    该用法可用于display方法和include标签

--- ThinkPHP/Lib/Behavior/LocationTemplateBehavior.class.php
+++ ThinkPHP/Lib/Behavior/LocationTemplateBehavior.class.php
@@ -40,17 +40,18 @@ class LocationTemplateBehavior extends Behavior {
         if(''==$templateFile) {
             // 如果模板文件名为空 按照默认规则定位
             $templateFile = C('TEMPLATE_NAME');
-        }elseif(false === strpos($templateFile,'.')){
-            $templateFile  = str_replace(':','/',$templateFile);
-            $count   =  substr_count($templateFile,'/');
-            $path   = dirname(C('TEMPLATE_NAME'));
-            if(0==$count && defined('GROUP_NAME') && '/' != C('TMPL_FILE_DEPR')) {
-                $templateFile  =  MODULE_NAME.C('TMPL_FILE_DEPR').$templateFile;
+        }elseif(false === strpos($templateFile,C('TMPL_TEMPLATE_SUFFIX'))){
+            // 解析规则为 模板主题:模块:操作 不支持 跨项目和跨分组调用
+            $path   =  explode(':',$templateFile);
+            $action = array_pop($path);
+            $module = !empty($path)?array_pop($path):MODULE_NAME;
+            if(!empty($path)) {// 设置模板主题
+                $path = dirname(APP_TMPL_PATH).'/'.array_pop($path).'/';
             }else{
-                for($i=0;$i<$count;$i++)
-                    $path   = dirname($path);
+                $path = APP_TMPL_PATH;
             }
-            $templateFile =  $path.'/'.$templateFile.C('TMPL_TEMPLATE_SUFFIX');
+            $depr = defined('GROUP_NAME')?C('TMPL_FILE_DEPR'):'/';
+            $templateFile  =  $path.$module.$depr.$action.C('TMPL_TEMPLATE_SUFFIX');
         }
         if(!file_exists_case($templateFile))
             throw_exception(L('_TEMPLATE_NOT_EXIST_').'['.$templateFile.']');
