a00edd53846dd226161168c3207f83c65eb9c768
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-01-02 15:26:36 +0000

    2.2版本Dispatcher类移回Util目录

--- ThinkPHP/Common/core.php
+++ ThinkPHP/Common/core.php
@@ -16,7 +16,7 @@ return array(
     THINK_PATH.'/Lib/Think/Core/Think.class.php',
     THINK_PATH.'/Lib/Think/Exception/ThinkException.class.php',  // 异常处理类
     THINK_PATH.'/Lib/Think/Core/Log.class.php',    // 日志处理类
-    THINK_PATH.'/Lib/Think/Core/Dispatcher.class.php', // URL调度和路由类
+    THINK_PATH.'/Lib/Think/Util/Dispatcher.class.php', // URL调度和路由类
     THINK_PATH.'/Lib/Think/Core/App.class.php',   // 应用程序类
     THINK_PATH.'/Lib/Think/Core/Action.class.php', // 控制器类
     //THINK_PATH.'/Lib/Think/Core/Model.class.php', // 模型类
