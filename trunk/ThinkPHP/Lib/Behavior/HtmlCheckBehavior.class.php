1ff5af085190999220f92de411488bc85431b548
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-01-03 05:33:10 +0000

    内置新的静态缓存行为 无需定义任何静态缓存规则，完全根据URL地址生成。 包含两种静态缓存访问方式： 1 纯静态访问（需要自己控制重新生成） 2 动态请求读取静态文件（支持静态缓存有效期）
    例如
    URL访问地址
    http://serverName/Html/News/1.html
    如果开启静态缓存 并且Html是静态缓存目录 .html 是静态缓存后缀 则会自动生成静态缓存文件，第二次访问则为纯静态访问。
    如果再次访问 
    http://serverName/News/1.html
    则会判断是否存在静态缓存 并且在有效期则会读取静态文件输出
    如果静态缓存失效，则会重新生成。

---
+++ trunk/ThinkPHP/Lib/Behavior/HtmlCheckBehavior.class.php
@@ -0,0 +1,44 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2009 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+// $Id$
+
+/**
+ +------------------------------------------------------------------------------
+ * 系统行为扩展 静态缓存URL检测
+ +------------------------------------------------------------------------------
+ */
+class HtmlCheckBehavior extends Behavior {
+    protected $options   =  array(
+            'HTML_CACHE_ON'    => false, // 开启静态缓存
+            'HTML_FILE_SUFFIX' => '.shtml', // 静态URL后缀
+            'HTML_CACHE_TIME' => 60,   // 静态缓存有效期 秒 仅在动态访问有效
+        );
+    public function run(&$params='') {
+        if(C('HTML_CACHE_ON') && $_SERVER['PATH_INFO']) {
+            $part =  pathinfo($_SERVER['PATH_INFO']);
+            if(isset($part['extension']) && $part['extension'] == ltrim(C('HTML_FILE_SUFFIX'),'.')) {
+                $htmlFile = HTML_PATH.ltrim($_SERVER['PATH_INFO'],'/');
+                if(is_file($htmlFile) && filemtime($htmlFile)<time()-C('HTML_CACHE_TIME')) {
+                    readfile($htmlFile);
+                    exit;
+                }else{
+                    $paths = explode('/',ltrim($_SERVER['PATH_INFO'],'/'));
+                    if($paths[0]==basename(HTML_PATH)) {
+                        // 开启静态缓存 则过滤URL地址中的静态目录名称
+                        array_shift($paths);
+                        $_SERVER['PATH_INFO'] =  implode('/',$paths);
+                        define('__HTML__',$_SERVER['PATH_INFO']);
+                    }
+                }
+            }
+        }
+    }
+}
