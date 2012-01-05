10c3af713202ea7808fb329f3d9ebba7b3dcfadb
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-01-05 03:45:27 +0000

    修改TMPL_PARSE_STRING配置的默认值为空数组

--- trunk/ThinkPHP/Lib/Behavior/ParseTemplateBehavior.class.php
+++ trunk/ThinkPHP/Lib/Behavior/ParseTemplateBehavior.class.php
@@ -27,7 +27,7 @@ class ParseTemplateBehavior extends Behavior {
         'TMPL_CACHFILE_SUFFIX'  => '.php',      // 默认模板缓存后缀
         'TMPL_DENY_FUNC_LIST'	=> 'echo,exit',	// 模板引擎禁用函数
         'TMPL_DENY_PHP'  =>false, // 默认模板引擎是否禁用PHP原生代码
-        'TMPL_PARSE_STRING'     => '',          // 模板引擎要自动替换的字符串，必须是数组形式。
+        'TMPL_PARSE_STRING'     => array(),          // 模板引擎要自动替换的字符串，必须是数组形式。
         'TMPL_L_DELIM'          => '{',			// 模板引擎普通标签开始标记
         'TMPL_R_DELIM'          => '}',			// 模板引擎普通标签结束标记
         'TMPL_VAR_IDENTIFY'     => 'array',     // 模板变量识别。留空自动判断,参数为'obj'则表示对象
