e2439c91841c5e61b242defe2caa95263718c05c
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-01-05 05:20:04 +0000

    2.2版本验证码改进

--- branches/2.2/AddOns/Library/ORG/Util/Image.class.php
+++ branches/2.2/AddOns/Library/ORG/Util/Image.class.php
@@ -326,9 +326,9 @@ class Image extends Think {//类定义开始
         $_SESSION[$verifyName] = md5($randval);
         $width = ($length * 10 + 10) > $width ? $length * 10 + 10 : $width;
         if ($type != 'gif' && function_exists('imagecreatetruecolor')) {
-            $im = @imagecreatetruecolor($width, $height);
+            $im = imagecreatetruecolor($width, $height);
         } else {
-            $im = @imagecreate($width, $height);
+            $im = imagecreate($width, $height);
         }
         $r = Array(225, 255, 255, 223);
         $g = Array(225, 236, 237, 255);
@@ -337,24 +337,20 @@ class Image extends Think {//类定义开始
 
         $backColor = imagecolorallocate($im, $r[$key], $g[$key], $b[$key]);    //背景色（随机）
         $borderColor = imagecolorallocate($im, 100, 100, 100);                    //边框色
-        $pointColor = imagecolorallocate($im, mt_rand(0, 255), mt_rand(0, 255), mt_rand(0, 255));                 //点颜色
 
-        @imagefilledrectangle($im, 0, 0, $width - 1, $height - 1, $backColor);
-        @imagerectangle($im, 0, 0, $width - 1, $height - 1, $borderColor);
+        imagefilledrectangle($im, 0, 0, $width - 1, $height - 1, $backColor);
+        imagerectangle($im, 0, 0, $width - 1, $height - 1, $borderColor);
         $stringColor = imagecolorallocate($im, mt_rand(0, 200), mt_rand(0, 120), mt_rand(0, 120));
         // 干扰
         for ($i = 0; $i < 10; $i++) {
-            $fontcolor = imagecolorallocate($im, mt_rand(0, 255), mt_rand(0, 255), mt_rand(0, 255));
-            imagearc($im, mt_rand(-10, $width), mt_rand(-10, $height), mt_rand(30, 300), mt_rand(20, 200), 55, 44, $fontcolor);
+            imagearc($im, mt_rand(-10, $width), mt_rand(-10, $height), mt_rand(30, 300), mt_rand(20, 200), 55, 44, $stringColor);
         }
         for ($i = 0; $i < 25; $i++) {
-            $fontcolor = imagecolorallocate($im, mt_rand(0, 255), mt_rand(0, 255), mt_rand(0, 255));
-            imagesetpixel($im, mt_rand(0, $width), mt_rand(0, $height), $pointColor);
+            imagesetpixel($im, mt_rand(0, $width), mt_rand(0, $height), $stringColor);
         }
         for ($i = 0; $i < $length; $i++) {
             imagestring($im, 5, $i * 10 + 5, mt_rand(1, 8), $randval{$i}, $stringColor);
         }
-//        @imagestring($im, 5, 5, 3, $randval, $stringColor);
         Image::output($im, $type);
     }
 
