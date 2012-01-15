1fabf8b1c9e8ba872a2a6829a9679380110cce1f
luofei614@gmail.com <luofei614@gmail.com@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-01-15 07:21:55 +0000

    删除不用的配置

--- branches/SAE/Sae/convention_sae.php
+++ branches/SAE/Sae/convention_sae.php
@@ -8,11 +8,5 @@ return array(
 	'DB_PWD'=> SAE_MYSQL_PASS,         // 密码
 	'DB_PORT'=> SAE_MYSQL_PORT,        // 端口
 	'DB_RW_SEPARATE'=>true,
-        'DB_DEPLOY_TYPE'=> 1, // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
-        'DATA_CACHE_TYPE'=> 'Memcache',//S缓存类型为Memcache
-        'HTML_FILE_SUFFIX'=>'.html',//默认静态文件后缀为html，在storage只有html文件能被直接浏览。
-        'SAE_THINK_DOMAIN'=>'think',//ThinkPHP系统所需storage的domain名称。用于存储日志和静态缓存等。
-        //sae下，默认不记录日志，如果要开启日志，请注释下面两行。开启日志功能后需要建立系统所需的storage。
-        'LOG_EXCEPTION_RECORD'=>false,
-        'LOG_RECORD'=>false
+        'DB_DEPLOY_TYPE'=> 1 // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
         );
