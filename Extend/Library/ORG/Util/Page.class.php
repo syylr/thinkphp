4a2af6ce3a502a93923ac49dbe46cac800ac43db
tdweb2u <tdweb2u@gmail.com>
2012-01-17 08:27:02 +0000

    修正默认C('VAR_PAGE')为page，防止配置文件中VAR_PAGE不存在的时候，出错。

--- Extend/Library/ORG/Util/Page.class.php
+++ Extend/Library/ORG/Util/Page.class.php
@@ -29,6 +29,8 @@ class Page {
     protected $coolPages   ;
     // 分页显示定制
     protected $config  =	array('header'=>'条记录','prev'=>'上一页','next'=>'下一页','first'=>'第一页','last'=>'最后一页','theme'=>' %totalRow% %header% %nowPage%/%totalPage% 页 %upPage% %downPage% %first%  %prePage%  %linkPage%  %nextPage% %end%');
+    // 默认分页变量名
+    protected $varPage;
 
     /**
      +----------------------------------------------------------
@@ -44,12 +46,13 @@ class Page {
     public function __construct($totalRows,$listRows='',$parameter='') {
         $this->totalRows = $totalRows;
         $this->parameter = $parameter;
+        $this->varPage = C('VAR_PAGE') ? C('VAR_PAGE') : 'page' ;
         if(!empty($listRows)) {
             $this->listRows = intval($listRows);
         }
         $this->totalPages = ceil($this->totalRows/$this->listRows);     //总页数
         $this->coolPages  = ceil($this->totalPages/$this->rollPage);
-        $this->nowPage  = !empty($_GET[C('VAR_PAGE')])?intval($_GET[C('VAR_PAGE')]):1;
+        $this->nowPage  = !empty($_GET[$this->varPage])?intval($_GET[$this->varPage]):1;
         if(!empty($this->totalPages) && $this->nowPage>$this->totalPages) {
             $this->nowPage = $this->totalPages;
         }
@@ -71,7 +74,7 @@ class Page {
      */
     public function show() {
         if(0 == $this->totalRows) return '';
-        $p = C('VAR_PAGE');
+        $p = $this->varPage;
         $nowCoolPage      = ceil($this->nowPage/$this->rollPage);
         $url  =  $_SERVER['REQUEST_URI'].(strpos($_SERVER['REQUEST_URI'],'?')?'':"?").$this->parameter;
         $parse = parse_url($url);
