b9f4b5eb095c72957e18ab7dd110c72448716a49
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-01-18 16:03:08 +0000

    修正Widget类对于think引擎的判断

--- ThinkPHP/Lib/Core/Widget.class.php
+++ ThinkPHP/Lib/Core/Widget.class.php
@@ -69,6 +69,17 @@ abstract class Widget {
             if(!empty($var)) extract($var, EXTR_OVERWRITE);
             // 直接载入PHP模板
             include $templateFile;
+        }elseif('think'==$template){ // 采用Think模板引擎
+            if($this->checkCache($templateFile)) { // 缓存有效
+                // 分解变量并载入模板缓存
+                extract($var, EXTR_OVERWRITE);
+                //载入模版缓存文件
+                include C('CACHE_PATH').md5($templateFile).C('TMPL_CACHFILE_SUFFIX');
+            }else{
+                $tpl = Think::instance('ThinkTemplate');
+                // 编译并加载模板文件
+                $tpl->fetch($templateFile,$var);
+            }
         }else{
             $class   = 'Template'.ucwords($template);
             if(is_file(CORE_PATH.'Driver/Template/'.$class.'.class.php')) {
@@ -83,5 +94,34 @@ abstract class Widget {
         }
         $content = ob_get_clean();
         return $content;
+    }
+
+    /**
+     +----------------------------------------------------------
+     * 检查缓存文件是否有效
+     * 如果无效则需要重新编译
+     +----------------------------------------------------------
+     * @access public
+     +----------------------------------------------------------
+     * @param string $tmplTemplateFile  模板文件名
+     +----------------------------------------------------------
+     * @return boolen
+     +----------------------------------------------------------
+     */
+    protected function checkCache($tmplTemplateFile) {
+        if (!C('TMPL_CACHE_ON')) // 优先对配置设定检测
+            return false;
+        $tmplCacheFile = C('CACHE_PATH').md5($tmplTemplateFile).C('TMPL_CACHFILE_SUFFIX');
+        if(!is_file($tmplCacheFile)){
+            return false;
+        }elseif (filemtime($tmplTemplateFile) > filemtime($tmplCacheFile)) {
+            // 模板文件如果有更新则缓存需要更新
+            return false;
+        }elseif (C('TMPL_CACHE_TIME') != 0 && time() > filemtime($tmplCacheFile)+C('TMPL_CACHE_TIME')) {
+            // 缓存是否在有效期
+            return false;
+        }
+        // 缓存有效
+        return true;
     }
 }
