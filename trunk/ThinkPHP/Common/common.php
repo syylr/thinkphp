137aa8a26a2ed771b1cd443b666f922181df0c3f
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-01-20 05:02:53 +0000

    优化import方法的文件导入 如果存在类名冲突则不导入

--- trunk/ThinkPHP/Common/common.php
+++ trunk/ThinkPHP/Common/common.php
@@ -138,12 +138,10 @@ function import($class, $baseUrl = '', $ext='.class.php') {
     if (substr($baseUrl, -1) != "/")
         $baseUrl .= "/";
     $classfile = $baseUrl . $class . $ext;
-    if (class_exists(basename($class),false) ) {
-        // 冲突检测
-        throw_exception(L('_CLASS_CONFLICT_') . ':' . basename($class));
+    if (!class_exists(basename($class),false)) {
+        // 如果类不存在 则导入类库文件
+        return require_cache($classfile);
     }
-    //导入目录下的指定类库文件
-    return require_cache($classfile);
 }
 
 /**
