cd60d1e060225cea1487e7f4800f5dd1a39e913f
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-01-23 13:45:17 +0000

    去掉 DB_SUFFIX 配置

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -60,7 +60,6 @@ return  array(
 	'DB_PWD'                => '',          // 密码
 	'DB_PORT'               => '',        // 端口
 	'DB_PREFIX'             => 'think_',    // 数据库表前缀
-	'DB_SUFFIX'             => '',          // 数据库表后缀
     'DB_FIELDTYPE_CHECK'    => false,       // 是否进行字段类型检查
     'DB_FIELDS_CACHE'       => true,        // 启用字段缓存
     'DB_CHARSET'            => 'utf8',      // 数据库编码默认采用utf8
