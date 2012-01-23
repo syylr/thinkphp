b7d75d7680089b96ec4df77656902741647dcebb
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-01-23 15:04:24 +0000

    系统内置行为的配置参数去掉不用或者多余的

--- ThinkPHP/Lib/Behavior/ParseTemplateBehavior.class.php
+++ ThinkPHP/Lib/Behavior/ParseTemplateBehavior.class.php
@@ -20,14 +20,10 @@ class ParseTemplateBehavior extends Behavior {
     // 行为参数定义（默认值） 可在项目配置中覆盖
     protected $options   =  array(
         // 布局设置
-        'LAYOUT_ON'           => false, // 是否启用布局
-        'LAYOUT_NAME'       => 'layout', // 当前布局名称 默认为layout
-
         'TMPL_ENGINE_TYPE'		=> 'Think',     // 默认模板引擎 以下设置仅对使用Think模板引擎有效
         'TMPL_CACHFILE_SUFFIX'  => '.php',      // 默认模板缓存后缀
         'TMPL_DENY_FUNC_LIST'	=> 'echo,exit',	// 模板引擎禁用函数
         'TMPL_DENY_PHP'  =>false, // 默认模板引擎是否禁用PHP原生代码
-        'TMPL_PARSE_STRING'     => array(),          // 模板引擎要自动替换的字符串，必须是数组形式。
         'TMPL_L_DELIM'          => '{',			// 模板引擎普通标签开始标记
         'TMPL_R_DELIM'          => '}',			// 模板引擎普通标签结束标记
         'TMPL_VAR_IDENTIFY'     => 'array',     // 模板变量识别。留空自动判断,参数为'obj'则表示对象
@@ -35,6 +31,8 @@ class ParseTemplateBehavior extends Behavior {
         'TMPL_CACHE_ON'			=> true,        // 是否开启模板编译缓存,设为false则每次都会重新编译
         'TMPL_CACHE_TIME'		=>	 0,         // 模板缓存有效期 0 为永久，(以数字为值，单位:秒)
         'TMPL_LAYOUT_ITEM'    =>   '{__CONTENT__}', // 布局模板的内容替换标识
+        'LAYOUT_ON'           => false, // 是否启用布局
+        'LAYOUT_NAME'       => 'layout', // 当前布局名称 默认为layout
 
         // Think模板引擎标签库相关设定
         'TAGLIB_BEGIN'          => '<',  // 标签库标签开始标记
