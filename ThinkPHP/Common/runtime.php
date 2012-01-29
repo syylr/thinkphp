f60530a2a4f0d9b5074638686a30a6485e5f3629
liu21st <liu21st@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-01-29 15:07:08 +0000

    改进import的项目类库目录判断 取消LIB_DIR常量

--- ThinkPHP/Common/runtime.php
+++ ThinkPHP/Common/runtime.php
@@ -58,22 +58,21 @@ if(!IS_CLI) {
 }
 
 // 路径设置 可在入口文件中重新定义 所有路径常量都必须以/ 结尾
+if(!defined('CORE_PATH')) define('CORE_PATH',THINK_PATH.'Lib/'); // 系统核心类库目录
+if(!defined('EXTEND_PATH')) define('EXTEND_PATH',THINK_PATH.'Extend/'); // 系统扩展目录
+if(!defined('MODE_PATH')) define('MODE_PATH',EXTEND_PATH.'Mode/'); // 系统模式目录
+if(!defined('VENDOR_PATH')) define('VENDOR_PATH',EXTEND_PATH.'Vendor/'); // 第三方类库目录
+if(!defined('LIBRARY_PATH')) define('LIBRARY_PATH',EXTEND_PATH.'Library/'); // 扩展类库目录
 if(!defined('COMMON_PATH')) define('COMMON_PATH',    APP_PATH.'Common/'); // 项目公共目录
-if(!defined('LIB_DIR')) define('LIB_DIR',      'Lib');
-if(!defined('LIB_PATH')) define('LIB_PATH',    APP_PATH.LIB_DIR.'/'); //
-if(!defined('CACHE_PATH')) define('CACHE_PATH',   RUNTIME_PATH.'Cache/'); //
-if(!defined('CONF_PATH')) define('CONF_PATH',  APP_PATH.'Conf/'); //
-if(!defined('LOG_PATH')) define('LOG_PATH',  RUNTIME_PATH.'Logs/'); //
-if(!defined('LANG_PATH')) define('LANG_PATH', APP_PATH.'Lang/'); //
-if(!defined('TEMP_PATH')) define('TEMP_PATH', RUNTIME_PATH.'Temp/'); //
-if(!defined('DATA_PATH')) define('DATA_PATH', RUNTIME_PATH.'Data/'); //
-if(!defined('CORE_PATH')) define('CORE_PATH',THINK_PATH.'Lib/');
-if(!defined('TMPL_PATH')) define('TMPL_PATH',APP_PATH.'Tpl/');
-if(!defined('HTML_PATH')) define('HTML_PATH',APP_PATH.'Html/');
-if(!defined('EXTEND_PATH')) define('EXTEND_PATH',THINK_PATH.'Extend/');
-if(!defined('MODE_PATH')) define('MODE_PATH',EXTEND_PATH.'Mode/');
-if(!defined('VENDOR_PATH')) define('VENDOR_PATH',EXTEND_PATH.'Vendor/');
-if(!defined('LIBRARY_PATH')) define('LIBRARY_PATH',EXTEND_PATH.'Library/');
+if(!defined('LIB_PATH')) define('LIB_PATH',    APP_PATH.'Lib/'); // 项目类库目录
+if(!defined('CONF_PATH')) define('CONF_PATH',  APP_PATH.'Conf/'); // 项目配置目录
+if(!defined('LANG_PATH')) define('LANG_PATH', APP_PATH.'Lang/'); // 项目语言包目录
+if(!defined('TMPL_PATH')) define('TMPL_PATH',APP_PATH.'Tpl/'); // 项目模板目录
+if(!defined('HTML_PATH')) define('HTML_PATH',APP_PATH.'Html/'); // 项目静态目录
+if(!defined('LOG_PATH')) define('LOG_PATH',  RUNTIME_PATH.'Logs/'); // 项目日志目录
+if(!defined('TEMP_PATH')) define('TEMP_PATH', RUNTIME_PATH.'Temp/'); // 项目缓存目录
+if(!defined('DATA_PATH')) define('DATA_PATH', RUNTIME_PATH.'Data/'); // 项目数据目录
+if(!defined('CACHE_PATH')) define('CACHE_PATH',   RUNTIME_PATH.'Cache/'); // 项目模板缓存目录
 
 // 加载运行时所需要的文件 并负责自动目录生成
 function load_runtime_file() {
