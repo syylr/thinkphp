894f8f74685b3689eb66fc7b8106c575f11b6f95
liu21st <liu21st@gmail.com>
2012-04-02 15:21:05 +0000

    [develop]日志记录加上IP地址

--- ThinkPHP/Lib/Core/Log.class.php
+++ ThinkPHP/Lib/Core/Log.class.php
@@ -93,7 +93,7 @@ class Log {
             $destination   =   $destination?$destination:C('LOG_DEST');
             $extra   =  $extra?$extra:C('LOG_EXTRA');
         }
-        error_log('------REQUEST_URI:'.$_SERVER['REQUEST_URI']."------\r\n".implode('',self::$log)."\r\n", $type,$destination ,$extra);
+        error_log('------'.get_client_ip().' REQUEST_URI:'.$_SERVER['REQUEST_URI']."------\r\n".implode('',self::$log)."\r\n", $type,$destination ,$extra);
         // 保存后清空日志缓存
         self::$log = array();
         //clearstatcache();
@@ -128,7 +128,7 @@ class Log {
             $destination   =   $destination?$destination:C('LOG_DEST');
             $extra   =  $extra?$extra:C('LOG_EXTRA');
         }
-        error_log("{$now} ".$_SERVER['REQUEST_URI']." | {$level}: {$message}\r\n", $type,$destination,$extra );
+        error_log("{$now} {$level}: {$message}\r\n", $type,$destination,$extra );
         //clearstatcache();
     }
 }
