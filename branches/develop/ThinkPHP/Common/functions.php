c61347d9265e72ceb578269f0f5d1e19a57c4ce0
liu21st@gmail.com <liu21st@gmail.com@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-04-11 13:30:20 +0000

    [develop]修正is_ssl一处错误

--- branches/develop/ThinkPHP/Common/functions.php
+++ branches/develop/ThinkPHP/Common/functions.php
@@ -255,7 +255,7 @@ function U($url='',$vars='',$suffix=true,$redirect=false,$domain=false) {
 
 // 判断是否SSL协议
 function is_ssl() {
-    if(isset($_SERVER['HTTPS']) && ('1' == $_SERVER['HTTPS'] || 'on' == strtolower($_SERVER['HTTPS'])){
+    if(isset($_SERVER['HTTPS']) && ('1' == $_SERVER['HTTPS'] || 'on' == strtolower($_SERVER['HTTPS']))){
         return true;
     }elseif(isset($_SERVER['SERVER_PORT']) && ('443' == $_SERVER['SERVER_PORT'] )) {
         return true;
