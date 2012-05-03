a91ef442d2adb38ed42a063564fd97401ec5ffe5
liu21st <liu21st@gmail.com>
2012-05-03 14:40:39 +0000

    [develop]模型的数组条件表达式支持非法字段过滤

--- ThinkPHP/Lib/Core/Model.class.php
+++ ThinkPHP/Lib/Core/Model.class.php
@@ -664,11 +664,15 @@ class Model {
         $options['model'] =  $this->name;
 
         // 字段类型验证
-        if(isset($options['where']) && is_array($options['where'])) {
+        if(isset($options['where']) && is_array($options['where']) && !empty($this->fields)) {
             // 对数组查询条件进行字段类型检查
             foreach ($options['where'] as $key=>$val){
-                if(in_array($key,$this->fields,true) && is_scalar($val)){
-                    $this->_parseType($options['where'],$key);
+                if(in_array($key,$this->fields,true)){
+                    if(is_scalar($val)) {
+                        $this->_parseType($options['where'],$key);
+                    }
+                }elseif('_' != substr($key,0,1)){
+                    unset($options['where'][$key]);
                 }
             }
         }
