9e8a588293ed290e605d49de3fe4d877f7c2d31c
liu21st@gmail.com <liu21st@gmail.com@539fd3ec-2725-0410-b7ab-251e373a8e33>
2012-07-15 04:22:44 +0000

    改进关联删除的关联条件写法

--- trunk/Extend/Model/RelationModel.class.php
+++ trunk/Extend/Model/RelationModel.class.php
@@ -275,14 +275,10 @@ class RelationModel extends Model {
                         }else{
                             $mappingFk   =   !empty($val['foreign_key'])?$val['foreign_key']:strtolower($this->name).'_id';     //  关联外键
                         }
-                        if($opType == 'DEL') {
-                            if(!empty($val['condition'])) {
-                                $mappingCondition   =   $val['condition'];
-                            }else{
-                                $mappingCondition[$mappingFk]   =   $pk;
-                            }
+                        if(!empty($val['condition'])) {
+                            $mappingCondition   =   $val['condition'];
                         }else{
-                            $mappingCondition = !empty($val['condition'])?  $val['condition'] :  "{$mappingFk}='{$pk}'";
+                            $mappingCondition[$mappingFk]   =   $pk;
                         }
                         // 获取关联model对象
                         $model = D($mappingClass);
