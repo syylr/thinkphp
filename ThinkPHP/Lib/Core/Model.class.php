00ebe1472be20ac874781465a2458947cc222da5
luofei614 <luofei614@gmail.com>
2012-08-02 03:49:53 +0000

    增加connection属性的声明

--- ThinkPHP/Lib/Core/Model.class.php
+++ ThinkPHP/Lib/Core/Model.class.php
@@ -42,6 +42,8 @@ class Model {
     protected $name = '';
     // 数据库名称
     protected $dbName  = '';
+    //数据库配置
+    protected $connection='';
     // 数据表名（不包含表前缀）
     protected $tableName = '';
     // 实际数据表名（包含表前缀）
